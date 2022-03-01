cd39912dd62042be4cb75974d929082990574d74
alvinfebrianto <90168547+alvinfebrianto@users.noreply.github.com>
2022-03-01 20:46:35 +0700

    Create AlvinFebrianto_031_[1] Fungsi yang Tidak Mengembalikan Nilai.cpp

---
+++ Praktikum 5/AlvinFebrianto_031_[1] Fungsi yang Tidak Mengembalikan Nilai.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+
+void garis();									            // deklarasi fungsi
+main() {
+	system("cls");
+	garis();
+	printf("NO    NIM    NAMA    NILAI \n");
+	garis();
+	
+	getch();
+}
+
+void garis() {							          		// definisi fungsi
+	printf("========================== \n");
+}
