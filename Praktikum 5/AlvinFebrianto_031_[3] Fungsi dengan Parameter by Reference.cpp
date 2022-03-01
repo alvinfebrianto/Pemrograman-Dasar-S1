fdf7f24cdfc4dd654b0f8f9bb7745b1c26787666
alvinfebrianto <90168547+alvinfebrianto@users.noreply.github.com>
2022-03-01 20:47:25 +0700

    Add files via upload

---
+++ Praktikum 5/AlvinFebrianto_031_[3] Fungsi dengan Parameter by Reference.cpp
@@ -0,0 +1,37 @@
+// Program Dua Puluh Enam
+// Program tukar menggunakan fungsi dengan parameter by reference
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+void tukar(int *px, int *py);							// deklarasi fungsi
+main() {
+	int a,b;
+	
+	system("cls");
+	a=80;
+	b=11;
+	
+	printf("Nilai sebelum pemanggilan fungsi \n");
+	printf("a = %i \n",a);
+	printf("b = %i \n",b);
+	tukar(&a,&b);
+	printf("Nilai setelah pemanggilan fungsi \n");
+	printf("a = %i \n",a);
+	printf("b = %i \n",b);
+	
+	getch();
+}
+
+void tukar(int *px, int *py) {						// definisi fungsi
+	int z;
+	
+	z=*px;
+	*px=*py;
+	*py=z;
+	
+	printf("Nilai di akhir fungsi \n");
+	printf("px = %i \n",*px);
+	printf("py = %i \n",*py);
+}
